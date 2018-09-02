ce475fcf8c2747a37565bd4eada9e7bdba91db2e
jamal hassouni <jamalhassouni13@gmail.com>
2018-09-02 23:06:25 +0100

    fix:Fix Drop And Drag todo item

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,15 +4,7 @@ const browserHandler = {
   chrome: () => <div className="cover-bar" />,
   firefox: () => <div className="cover-bar width-15" />
 };
-var cachedTodos;
-var Todos = {
-  data: [
-    { id: 1, item: "php" },
-    { id: 2, item: "javascript" },
-    { id: 3, item: "react" },
-    { id: 4, item: "vue" },
-  ]
-};
+
 class TodoItem extends Component {
   constructor(props) {
     super(props);
@@ -22,9 +14,6 @@ class TodoItem extends Component {
   }
 
   render() {
-    localStorage.getItem("todos") == null
-      ? (cachedTodos = [])
-      : (cachedTodos = JSON.parse(localStorage.getItem("todos")));
     var removeSVG =
       '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
     var completeSVG =
@@ -35,7 +24,7 @@ class TodoItem extends Component {
         // eslint-disable-next-line
         const dragging = index == this.state.dragging ? "dragging" : "";
         // eslint-disable-next-line
-        if (data.id == this.state.idEdited) {
+        if (data.id === this.state.idEdited) {
           return (
             <input
               className="inputedit"
@@ -97,7 +86,7 @@ class TodoItem extends Component {
     var idEdited = this.state.idEdited;
     if (charCode === 13) {
       console.log("id", this.state.idEdited);
-      var items = Todos.data;
+      var items = this.props.todos;
       if (idEdited !== -1) {
         items[idEdited] = this.refs.input.value;
         console.log("id updated", this.state.idEdited);
@@ -106,7 +95,6 @@ class TodoItem extends Component {
         todos: items,
         idEdited: null
       });
-      localStorage.setItem("todos", JSON.stringify(items));
     }
   }
   handleEdit(e) {
@@ -129,7 +117,7 @@ class TodoItem extends Component {
 
   handleDelete(e) {
     let id = e.currentTarget.parentNode.parentNode.dataset.id;
-    let item = Todos.data[id];
+    let item =this.props.todos[id].item;
     this.props.onDelete(item);
   }
   handleComplete(e) {
@@ -138,12 +126,11 @@ class TodoItem extends Component {
       console.log(e.currentTarget);
       console.log(e.target);
       let id = e.currentTarget.parentNode.parentNode.dataset.id;
-      let key = e.currentTarget.parentNode.parentNode.dataset.index;
-      console.log("key",key);
+      console.log("id",id);
       this.setState({
         idEdited: id
       });
-      let item = this.props.todos[key].item;
+      let item = this.props.todos[id].item;
       console.log(item,id);
       this.props.onComplete(id);
     }
@@ -151,43 +138,44 @@ class TodoItem extends Component {
 
   dragOver(e) {
     e.preventDefault();
-    const items = cachedTodos;
-    const over = e.currentTarget;
+    const items = this.state.todos;
+    const key = e.currentTarget.dataset.index;
     const dragging = this.state.dragging;
     const from = isFinite(dragging) ? dragging : this.dragged;
-    let to = Number(over.dataset.id);
+    let to = Number(key);
     items.splice(to, 0, items.splice(from, 1)[0]);
     console.log("over to ", to);
     this.sort(items, to);
   }
 
-  dragEnd(e) {
+  dragEnd() {
     // update state
     this.sort(this.state.todos, undefined);
   }
 
-  dragEnter(e) {}
+  dragEnter() {}
 
-  dragLeave(e) {
+  dragLeave() {
     console.log("leave");
   }
   dragStart(e) {
+    const key = e.currentTarget.dataset.index;
     e.dataTransfer.effectAllowed = "move";
     e.dataTransfer.setData("text/plain", null);
-    this.dragged = Number(e.currentTarget.dataset.id);
-    console.log("id", this.dragged);
+    this.dragged = Number(key);
+    console.log("id draged", this.dragged);
   }
   Drop(e) {
     e.preventDefault();
-    const items = cachedTodos;
-    const over = e.currentTarget;
+    const items = this.props.todos;
+    const key = e.currentTarget.dataset.index;
     const dragging = this.state.dragging;
     const from = isFinite(dragging) ? dragging : this.dragged;
-    let to = Number(over.dataset.id);
+    let to = Number(key);
     items.splice(to, 0, items.splice(from, 1)[0]);
     this.sort(items, to);
     console.log("data droped", items);
-    console.log("droped to", e.currentTarget.dataset.id);
+    console.log("droped to", key);
     console.log("droped from ", this.dragged);
     console.log("droped");
   }
@@ -197,8 +185,6 @@ class TodoItem extends Component {
     state.todos = list;
     state.dragging = dragging;
     this.setState({ state });
-    // update localStorage
-    localStorage.setItem("todos", JSON.stringify(list));
   }
 
   componentDidMount() {}
