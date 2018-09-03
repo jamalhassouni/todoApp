c9902d7b33dba6daca6671f01797923e6e3ee142
jamal hassouni <jamalhassouni13@gmail.com>
2018-09-03 18:09:18 +0100

    fix: Fix the map loop when there is no data

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -19,6 +19,7 @@ class TodoItem extends Component {
     var completeSVG =
       '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
     var list = this.props.todos;
+    if (list != null) {
     list = list.map(
       function(data, index) {
         // eslint-disable-next-line
@@ -73,6 +74,8 @@ class TodoItem extends Component {
       }.bind(this)
     );
 
+
+   }
     return (
       <ul className="todo scroll-bar-wrap" id="todo">
         <div className="scroll-box">{list}</div>
