01a4b22412d43d70dd5e2c179f2bc306ed72f4f6
jamal hassouni <jamalhassouni13@gmail.com>
2018-09-04 15:31:19 +0100

    refactor:Wrap App in BrowserRouter component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import './reset.css';
 import App from './App';
@@ -7,5 +8,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 // put component into html page
 
-ReactDOM.render(<App  />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App  /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
