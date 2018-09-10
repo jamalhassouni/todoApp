2278399d4337526c3215137f2f807bea8637fbc2
jamal hassouni <jamalhassouni13@gmail.com>
2018-09-10 17:07:12 +0100

    refactor: Convert About to a Stateless Functional Component

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,47 +1,48 @@
-import React, { Component } from "react";
-import {Link } from 'react-router-dom';
-import Card from './Card';
-import HeaderComponent from './Header';
-import Contact from '../Contact/Contact';
-//import "../../utils/styles/materialize.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import Card from "./Card";
+import HeaderComponent from "./Header";
+import Contact from "../Contact/Contact";
 import "./About.css";
-class About  extends Component{
-  render(){
-     return(
-         <div>
-        <HeaderComponent/>
-         <Card/>
-         <div id="about" className="section">
-    <div className="container">
-        <div className="row">
+const About = () => {
+  return (
+    <div>
+      <HeaderComponent />
+      <Card />
+      <div id="about" className="section">
+        <div className="container">
+          <div className="row">
             <div className="col-md-12">
-                <div id="about-card" className="card">
-                    <div className="card-content">
-                        <p>
-                            Hello! I’m Jamal Hassouni. Senior Web Developer with over 5 years of experience
-                            specializing in front end development. Experienced with all stages of the
-                            development cycle for dynamic web projects.Having an in-depth knowledge
-                            including advanced HTML5, CSS, CSS3, SASS, PugJS, JSON, XML,PHP, JavaScript,
-                            JQuery, React JS.
-                        </p>
-                    </div>
-
-                    <div id="about-btn" className="card-action">
-                        <div className="about-btn">
-                            <Link to={'/'} className="btn waves-effect" >Home</Link>
-                            <a href='#contact' className="btn waves-effect">Contact Me</a>
-                        </div>
-                    </div>
+              <div id="about-card" className="card">
+                <div className="card-content">
+                  <p>
+                    Hello! I’m Jamal Hassouni. Senior Web Developer with over 5
+                    years of experience specializing in front end development.
+                    Experienced with all stages of the development cycle for
+                    dynamic web projects.Having an in-depth knowledge including
+                    advanced HTML5, CSS, CSS3, SASS, PugJS, JSON, XML,PHP,
+                    JavaScript, JQuery, React JS.
+                  </p>
                 </div>
+
+                <div id="about-btn" className="card-action">
+                  <div className="about-btn">
+                    <Link to={"/"} className="btn waves-effect">
+                      Home
+                    </Link>
+                    <a href="#contact" className="btn waves-effect">
+                      Contact Me
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
+      <Contact />
     </div>
-</div>
-<Contact/>
-        </div>
-     );
+  );
+};
 
-  }
-
-}
 export default About;
